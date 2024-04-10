7863f3bbb8913c4aa38707ca0452a14bf99149e5
ShalomLeonardoCarmo <shalomleonardocarmo@gmail.com>
2024-04-10 19:01:58 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="h-full">
-      <div className="bg-gradient-to-r from-sky-500 to-indigo-500 flex flex-1">
-        <span>udsfjhesgf</span>
+    <main className="h-screen">
+      <div className="bg-gradient-to-r from-sky-500 to-blue-950 flex flex-1 h-full">
+        <span className="text-8xl text-center">AS MELHORES FÉRIAS DA SUA VIDA</span>
       </div>
     </main>
   );
