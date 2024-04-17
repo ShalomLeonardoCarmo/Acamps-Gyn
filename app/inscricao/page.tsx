8c1dfee239703b2e510fd43ea01bc2a517cf1c53
ShalomLeonardoCarmo <shalomleonardocarmo@gmail.com>
2024-04-17 18:02:37 -0300

    feat: add initial forms

--- app/inscricao/page.tsx
+++ app/inscricao/page.tsx
@@ -1,58 +1,21 @@
 'use client'
 
-import axios from 'axios'
-import { useState } from 'react'
-
 export default function InscricaoPage() {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [productData, setProductData] = useState({
-    customer: {
-      name: 'khefuwgvef',
-      email: 'kshfhjb@teste.com',
-      tax_id: '61739303318',
-    },
-    reference_id: 'acampsgyn',
-    items: [
-      {
-        name: 'Acamps',
-        quantity: 1,
-        unit_amount: 27990,
-      },
-    ],
-    qr_codes: [{ amount: { value: '27990' } }],
-  })
-
-  function generateProductPix() {
-    const data = {}
-    axios
-      .post('https://api.pagseguro.com/oders', data, {
-        headers: {
-          'Access-Control-Allow-Origin': 'https://acampsgyn.vercel.app/',
-          Authorization:
-            'Bearer bbc30c59-06c1-4d83-a3ff-02b225f411f002045c104336ad028a086f72245306683ac8-c5fa-4d2b-a4ca-b20e4dd0b37b',
-          'Content-Type': 'application/json',
-        },
-        withCredentials: true,
-      })
-      .then((response) => {
-        setProductData(response.data)
-      })
-      .catch((error) => {
-        alert('Erro')
-        console.error(error)
-      })
-  }
-
   return (
-    <main className="flex flex-col">
-      <span>Essa é a página de inscrições</span>
-      <button
-        onClick={generateProductPix}
-        className="flex p-2 bg-blue-500 w-fit rounded-xl"
-      >
-        Clique aqui para testar
-      </button>
-      <pre>{String(productData)}</pre>
+    <main className="flex flex-col items-center gap-4">
+      <h1 className="hidden">Inscrições</h1>
+      <section className="flex flex-col mt-4">
+        <h2 className="font-bold text-4xl gap-2 text-purple-700">
+          INSCRIÇÃO PARTICIPANTE
+        </h2>
+        <span className="flex justify-center font-semibold">
+          Venha viver as melhores férias da sua vida!
+        </span>
+
+        <button className="font-bold p-2 bg-orange-400 text-white rounded-xl shadow-sm hover:bg-purple-700 transition-all hover:shadow-xl">
+          CLIQUE AQUI!
+        </button>
+      </section>
     </main>
   )
 }
