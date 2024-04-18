843a2c9ee9b9ce43392b7b9310f6a515827dbe39
ShalomLeonardoCarmo <shalomleonardocarmo@gmail.com>
2024-04-18 16:27:20 -0300

    add

--- app/database/create/table/users/route.ts
+++ app/database/create/table/users/route.ts
@@ -1,7 +1,7 @@
 import { sql } from '@vercel/postgres'
 import { NextResponse } from 'next/server'
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
     const result =
       await sql`CREATE TABLE users (name varchar(255) NOT NULL, birthdate date, cpf varchar(11), email varchar(255) NOT NULL, phone varchar(255), responsible_person_number varchar(255) NOT NULL, frequently_use_medication boolean NOT NULL, medication_name varchar(255), how_find_acamps varchar(255), wich_city SMALLESTINT NOT NULL, other_city_name varchar(255), my_friend_called_me varchar(255))`
