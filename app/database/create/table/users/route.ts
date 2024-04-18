e68d68a3fe7778b3f6b426f701cfb48a1b22d64b
ShalomLeonardoCarmo <shalomleonardocarmo@gmail.com>
2024-04-18 16:27:10 -0300

    update

--- app/database/create/table/users/route.ts
+++ app/database/create/table/users/route.ts
@@ -5,8 +5,8 @@ export async function GET(request: Request) {
   try {
     const result =
       await sql`CREATE TABLE users (name varchar(255) NOT NULL, birthdate date, cpf varchar(11), email varchar(255) NOT NULL, phone varchar(255), responsible_person_number varchar(255) NOT NULL, frequently_use_medication boolean NOT NULL, medication_name varchar(255), how_find_acamps varchar(255), wich_city SMALLESTINT NOT NULL, other_city_name varchar(255), my_friend_called_me varchar(255))`
-    return NextResponse.json({result}, {status: 201})
+    return NextResponse.json({ result }, { status: 201 })
   } catch (error) {
-    return NextResponse.json({error}, {status: 500})
+    return NextResponse.json({ error }, { status: 500 })
   }
 }
