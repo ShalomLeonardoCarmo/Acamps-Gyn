7803b28fd89e57a4271353f27317229dd18e423f
ShalomLeonardoCarmo <shalomleonardocarmo@gmail.com>
2024-04-23 19:44:31 -0300

    feat: add header in mobile

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link'
 
 export default function HeaderMenu() {
   return (
-    <nav className="items-center justify-evenly flex-1 text-yellow-200 md:flex hidden">
+    <nav className="items-center justify-evenly flex-1 text-yellow-200 flex divide-x">
       <button
         onClick={() => {
           alert('Liberado em breve. Por enquanto, que tal fazer sua inscrição?')
         }}
-        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
+        className="lg:hover:scale-125 justify-center items-center flex-[1] md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
       >
         Check-list
       </button>
@@ -17,13 +17,13 @@ export default function HeaderMenu() {
         onClick={() => {
           alert('Liberado em breve. Por enquanto, que tal fazer sua inscrição?')
         }}
-        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
+        className="lg:hover:scale-125 hidden justify-center items-center flex-[1] md:flex md:hover:scale-110 lg:text-xl text-md font-bold h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
       >
         Local
       </button>
       <Link
         href="/inscricao"
-        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
+        className="lg:hover:scale-125 md:hover:scale-110 justify-center items-center flex-[1] lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
       >
         Inscrição
       </Link>
@@ -31,7 +31,7 @@ export default function HeaderMenu() {
         onClick={() => {
           alert('Liberado em breve. Por enquanto, que tal fazer sua inscrição?')
         }}
-        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
+        className="lg:hover:scale-125 md:hover:scale-110 justify-center items-center flex-[1] lg:text-xl text-md font-bold hidden md:flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
       >
         Entre em contato
       </button>
@@ -39,7 +39,7 @@ export default function HeaderMenu() {
         onClick={() => {
           alert('Liberado em breve. Por enquanto, que tal fazer sua inscrição?')
         }}
-        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
+        className="lg:hover:scale-125 md:hover:scale-110 justify-center items-center flex-[1] lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
       >
         Apadrinhamento
       </button>
