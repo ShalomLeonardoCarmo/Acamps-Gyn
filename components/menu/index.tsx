0a8546a183d66ae72f954edfb03876d32854a9d5
ShalomLeonardoCarmo <shalomleonardocarmo@gmail.com>
2024-04-23 19:15:13 -0300

    feat: update header

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,38 +1,48 @@
+'use client'
+
 import Link from 'next/link'
 
 export default function HeaderMenu() {
   return (
     <nav className="items-center justify-evenly flex-1 text-yellow-200 md:flex hidden">
-      <Link
-        href="#checklist"
+      <button
+        onClick={() => {
+          alert('Liberado em breve. Por enquanto, que tal fazer sua inscrição?')
+        }}
         className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
       >
         Check-list
-      </Link>
-      <Link
-        href="#local"
+      </button>
+      <button
+        onClick={() => {
+          alert('Liberado em breve. Por enquanto, que tal fazer sua inscrição?')
+        }}
         className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
       >
         Local
-      </Link>
-      <Link
-        href="#apadrinhamento"
-        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
-      >
-        Apadrinhamento
-      </Link>
-      <Link
-        href="#contato"
-        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
-      >
-        Entre em contato
-      </Link>
+      </button>
       <Link
         href="/inscricao"
         className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
       >
         Inscrição
       </Link>
+      <button
+        onClick={() => {
+          alert('Liberado em breve. Por enquanto, que tal fazer sua inscrição?')
+        }}
+        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
+      >
+        Entre em contato
+      </button>
+      <button
+        onClick={() => {
+          alert('Liberado em breve. Por enquanto, que tal fazer sua inscrição?')
+        }}
+        className="lg:hover:scale-125 md:hover:scale-110 lg:text-xl text-md font-bold flex h-fit hover:text-yellow-300 focus:text-yellow-300 transition-all"
+      >
+        Apadrinhamento
+      </button>
     </nav>
   )
 }
