284b46faa88f03cafcd25ae39c5fafe03c90c4b3
ShalomLeonardoCarmo <shalomleonardocarmo@gmail.com>
2024-05-03 17:04:07 -0300

    fix:

--- app/api/database/create-servant/route.ts
+++ app/api/database/create-servant/route.ts
@@ -1,13 +1,10 @@
 import { createServant } from '@/services'
-import { internalAlertServant } from '@/services/mails'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
   const body = await new Response(request.body).text()
 
   await createServant(JSON.parse(body))
-
-  internalAlertServant()
 
   return NextResponse.json({ result: 'Sucesso' }, { status: 201 })
 }
