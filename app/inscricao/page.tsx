fcbd35c2b056c85a6ac54f55c819f1b57f1c8885
ShalomLeonardoCarmo <shalomleonardocarmo@gmail.com>
2024-07-04 18:42:47 -0300

    feat: add cancel policys

--- app/inscricao/page.tsx
+++ app/inscricao/page.tsx
@@ -50,6 +50,35 @@ export default function InscricaoPage() {
       />
 
       <ServoForm onClose={() => setOpenServo(false)} show={openServo} />
+
+      <div className="flex flex-col gap-3 mt-2">
+        <span>
+          <strong>REEMBOLSO:</strong> A solicitação de reembolso deverá ser
+          feita em até 15 dias que antecedem o evento, ou em caso de doença do
+          participante/falecimento de algum parente de primeiro grau. Para tal,
+          deve ser enviado um e-mail para Juventudegoiania@comshalom.org, com o
+          título REEMBOLSO ACAMP&apos;S, por meio do nome e email cadastrados no
+          momento da compra, acompanhado em anexo com atestado médico ou uma
+          certidão de óbito. Em até 10 dias úteis será encaminhado o pedido de
+          reembolso de ingressos no cartão de crédito ou em conta corrente com
+          mesma titularidade. A disponibilização do valor ocorrerá de acordo com
+          as regras da administradora do cartão ou banco do cliente.
+        </span>
+        <span>
+          <strong>CANCELAMENTOS: </strong>
+          De acordo com o Código de Defesa do Consumidor (Artigo 49), em caso de
+          desistência, que deverá ocorrer em, no máximo, 7 dias corridos após a
+          compra. O cliente deverá entrar em contato conosco através do e-mail
+          juventudegoiania@comshalom.org, com o título CANCELAMENTO DA INSCRIÇÃO
+          ACAMP’S, acompanhado pelo o nome e email cadastrados no momento da
+          compra, de segunda a sexta das 14:00 às 20:00 solicitando o
+          cancelamento do ingresso. Em até 10 dias úteis será encaminhado o
+          pedido de reembolso de ingressos no cartão de crédito ou em conta
+          corrente com mesma titularidade. A disponibilização do valor ocorrerá
+          de acordo com as regras da administradora do cartão ou banco do
+          cliente.
+        </span>
+      </div>
     </main>
   )
 }
